91632185164cea0c19bbed46d95da5b29fc078d8
sdkag <senyo.k.agawu@gmail.com>
2021-10-01 02:02:27 -0400

    add notes  slice of state

---
+++ frontend/src/store/notes.js
@@ -0,0 +1,47 @@
+import { fetch } from "./csrf.js";
+
+const FETCH_NOTES = fetch("/api/notes");
+
+const fetchNotes = (notes) => ({
+  type: FETCH_NOTES,
+  payload: notes,
+});
+
+const initialState = {
+  byId: {
+    1: {
+      title: "The Beginning",
+      body: "The beginning of the story",
+    },
+    2: {
+      title: "The Middle",
+    },
+    3: {
+      title: "The End",
+    },
+  },
+  allIds: [1, 2, 3],
+  archived: [3],
+  notes: [1, 2],
+  pinned: [2],
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_NOTES:
+      return {
+        byId: action.payload.reduce(
+          (state, note) => ({
+            ...state,
+            [note.id]: note,
+          }),
+          {}
+        ),
+        allIds: action.payload.map(({ id }) => id),
+      };
+    default:
+      return state;
+  }
+}
+
+export default reducer;
